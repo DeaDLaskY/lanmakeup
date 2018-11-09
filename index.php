acb7d9917b97c0c58ea64601fac3496457a174d7
DeaDLaskY <pologeek6@gmail.com>
2018-11-09 09:40:20 +0100

    9/11 9h40

--- index.php
+++
@@ -1,2 +0,0 @@
-Ton pseudo est: <?php echo $_POST['pseudo']?><br>
-Ton mot de pass est: <?php echo $_POST['password'] ?>
--- inscription.php
+++ inscription.php
@@ -1,6 +1,13 @@
 <?php
+ echo$_POST['mail']; echo "<br/>";
+echo $_POST['pseudo']; echo "<br/>";
+echo $_POST['mdp']; echo "<br/>";
+
+
 
 foreach($_POST['check_list'] as $values)
 {
-    echo $values;
+    echo $values; echo "<br/>";
 }
+
+if (isset($_POST['listelol'])) echo $_POST['listelol'];
