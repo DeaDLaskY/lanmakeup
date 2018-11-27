c9e4a56afa6bcaad7dbeee233f0072f6b5c1a24b
DeaDLaskY <pologeek6@gmail.com>
2018-11-27 21:21:40 +0100

    27/11 21h21

--- inscription.php
+++ inscription.php
@@ -1,15 +1,31 @@
 <?php
-echo $_POST['mail'];
-echo $_POST['pseudo'];
-echo $_POST['mdp'];
+echo $_POST['mail']; echo "<br>";
+echo $_POST['pseudo'];echo "<br>";
+echo $_POST['mdp'];echo "<br>";
 $mail = $_POST['mail'];
 $pseudo = $_POST['pseudo'];
 $mdp = $_POST['mdp'];
 
-foreach($_POST['check_list'] as $values)
-{
-    echo $values;
-}
+$lol = $_POST['LOL'];
+$csgo = $_POST['CSGO'];
+$rocketleague = $_POST['ROCKETLEAGUE'];
+$overwatch = $_POST['OVERWATCH'];
+$pubg = $_POST['PUBG'];
+$sc = $_POST['SC'];
+$bf = $_POST['BF'];
+$cod = $_POST['COD'];
+$battlerite = $_POST['BATTLERITE'];
+$fortnite = $_POST['FORTNITE'];
+$civ = $_POST['CIV'];
+
+
+
+
+
+
+
+
+
 
 $servername = "localhost";
 $username = "root";
@@ -20,12 +36,16 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
-    $query = "INSERT INTO membres (EMAIL,PSEUDO,PASSWORD,JEUX) VALUES (\"$mail\",\"$pseudo\",\"$mdp\",\"$values\")";
-    //"INSERT INTO contents (type, reporter, description)
-    //             VALUES('$type', '$reporter', 'whatever')";
+    echo "<br>";
+    $query = "INSERT INTO membres (EMAIL,PSEUDO,PASSWORD) VALUES (\"$mail\",\"$pseudo\",\"$mdp\")";
     $name1 = $conn->prepare($query);
     $conn->query($query);
     echo $query;
+    $jeux = "INSERT INTO jeux (PSEUDO,LOL,CSGO,ROCKETLEAGUE,OVERWATCH,PUBG,STARCRAFT) VALUES (\"$pseudo\",\"$lol\",\"$csgo\",\"$rocketleague\",\"$overwatch\",\"$pubg\",\"$sc\")";
+    $name2 = $conn->prepare($jeux);
+    $conn->query($jeux);
+    echo $jeux;
+
 
 }
 catch(PDOException $e)
